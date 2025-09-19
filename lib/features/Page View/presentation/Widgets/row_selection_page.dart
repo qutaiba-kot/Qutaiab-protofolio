e6e4bf377d093ba387ad5deba81eb8c74b1d5d59
Qutaiba Al-Barahmeh <qutibaone@gmail.com>
2025-09-19 06:22:10 +0300

    improving the row selection page ui and adding home text

--- lib/features/Page View/presentation/Widgets/row_selection_page.dart
+++ lib/features/Page View/presentation/Widgets/row_selection_page.dart
@@ -4,6 +4,7 @@ import 'package:protofolio/Core/Theme/app_colors.dart';
 import 'package:protofolio/features/Page%20View/Responsive%20helper/responsive_helper.dart';
 import 'package:protofolio/features/Page%20View/logic/cubit/page_view_navigation_state.dart';
 import 'package:protofolio/features/Page%20View/logic/cubit/page_view_navigation_cubit.dart';
+
 class RowSelectionPage extends StatelessWidget {
   final double width;
   final double height;
@@ -28,6 +29,7 @@ class RowSelectionPage extends StatelessWidget {
     );
 
     final navItems = [
+      {"title": "Home", "index": 0},
       {"title": "About", "index": 1},
       {"title": "Experience", "index": 2},
       {"title": "Projects", "index": 3},
@@ -93,21 +95,24 @@ class _NavItemState extends State<_NavItem> {
           builder: (context, hovering, child) {
             return AnimatedScale(
               scale: hovering ? 1.2 : 1.0,
-              duration: const Duration(milliseconds: 100),
+              duration: const Duration(milliseconds: 50),
               child: Text(
                 widget.title,
                 style: TextStyle(
                   fontSize: widget.fontSize,
-                  fontWeight:
-                      widget.isSelected ? FontWeight.bold : FontWeight.normal,
+                  fontWeight: widget.isSelected
+                      ? FontWeight.bold
+                      : FontWeight.normal,
                   color: widget.isSelected
-                      ?AppColors.primary
+                      ? AppColors.primary
                       : hovering
-                          ? Colors.grey
-                          : Colors.white,
-                  decoration:
-                      hovering ? TextDecoration.underline : TextDecoration.none,
-                  decorationThickness: 2,
+                      ? Colors.grey
+                      : Colors.white,
+                  decoration:  hovering
+                      ? TextDecoration.overline
+                      : TextDecoration.none,
+                  decorationThickness: 1,
+                  decorationColor: Colors.blue,
                 ),
               ),
             );
